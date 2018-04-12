8adeb848d96e79a1505ecd3ea4088a2c48b9bc1a
chokeslam <chokeslam5608@gmail.com>
2018-04-12 17:49:05 +0800

    db change

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'heroku_2dfd5949b9ca3ff'),
-            'username' => env('DB_USERNAME', 'b623e23f1091b1'),
-            'password' => env('DB_PASSWORD', '67197a13'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
